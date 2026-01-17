435998aa216077dd5ac678d3e2e7df505593acab
Abdi Musse <amusse@alumni.cmu.edu>
2026-01-17 11:47:04 -0600

    updates

--- index.js
+++ index.js
@@ -8,21 +8,17 @@ const deeplinks = JSON.parse(raw);
 
 async function fetchEvents() {
   try {
-    const response = await fetch('https://site.web.api.espn.com/apis/site/v2/guide/feed?leagues=nba');
+    const response = await fetch('https://site.web.api.espn.com/apis/site/v2/guide/feed?leagues=nba%2Cnfl%2Ceng.1');
     const data = await response.json();
-    const appleEvents = await fetchDeeplinks();
     
     // Create an object keyed by event ID instead of an array
     const eventsDictionary = data.events.reduce((acc, { id, displayName, watch }) => {
       acc[id] = {
         title: displayName,
-        broadcasts: watch.broadcasts.map(({ broadcasterId }) => {
+        deeplinks: watch.broadcasts.map(({ broadcasterId }) => {
           let deeplink = null;
           switch (broadcasterId) {
               case 763:
-                   const primeBroadcasts = appleEvents.find(e => e.title === displayName)?.broadcasts;
-                   const primeLinks = primeBroadcasts.find(b => b.id === "tvs.vds.13221");
-                   deeplink = primeLinks?.deeplink || ""
                    break
               case 126:
                    deeplink = `sportscenter://x-callback-url/showWatchStream?playGameID=${id}`
@@ -32,7 +28,7 @@ async function fetchEvents() {
               default:
                 console.log("NO MATCH")
           }
-          return { id: broadcasterId, deeplink };
+          return { broadcasterId: broadcasterId, deeplink };
         })
       };
       return acc;
@@ -47,9 +43,15 @@ async function fetchEvents() {
   }
 }
 
+
+//NBA:  https://tv.apple.com/api/uts/v3/shelves/uts.col.SportsRelated.umc.cse.67kkuyv8dsexy9dx1tvoj6ulh?caller=web&locale=en-US&pfm=web&sf=143441&utscf=OjAAAAEAAAAAAAIAEAAAACMAKwAtAA%7E%7E&utsk=6e3013c6d6fae3c2%3A%3A%3A%3A%3A%3A235656c069bb0efb&v=92
+
+//NFL:  https://tv.apple.com/api/uts/v3/shelves/uts.col.SportsRelated.umc.cse.5vvmqj7yumant3n5u51i58f8q?caller=web&locale=en-US&pfm=web&sf=143441&utscf=OjAAAAEAAAAAAAIAEAAAACMAKwAtAA%7E%7E&utsk=6e3013c6d6fae3c2%3A%3A%3A%3A%3A%3A235656c069bb0efb&v=92
+
+// EPL: https://tv.apple.com/api/uts/v3/shelves/uts.col.SportsRelated.umc.cse.22kkr4ha0jekxtcdlalgsj7b?caller=web&locale=en-US&pfm=web&sf=143441&utscf=OjAAAAEAAAAAAAIAEAAAACMAKwAtAA%7E%7E&utsk=6e3013c6d6fae3c2%3A%3A%3A%3A%3A%3A235656c069bb0efb&v=92
 async function fetchDeeplinks() {
   try {
-    const response = await fetch('https://tv.apple.com/api/uts/v3/shelves/uts.col.SportsRelated.umc.cse.67kkuyv8dsexy9dx1tvoj6ulh?caller=web&locale=en-US&pfm=web&sf=143441&utscf=OjAAAAEAAAAAAAIAEAAAACMAKwAtAA%7E%7E&utsk=6e3013c6d6fae3c2%3A%3A%3A%3A%3A%3A235656c069bb0efb&v=92');
+    const response = await fetch('https://tv.apple.com/api/uts/v3/shelves/uts.col.SportsRelated.umc.cse.22kkr4ha0jekxtcdlalgsj7b?caller=web&locale=en-US&pfm=web&sf=143441&utscf=OjAAAAEAAAAAAAIAEAAAACMAKwAtAA%7E%7E&utsk=6e3013c6d6fae3c2%3A%3A%3A%3A%3A%3A235656c069bb0efb&v=92');
     
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -78,14 +80,14 @@ const formatedEvents = data.data.shelf.items.map(item => {
   };
 });
 
-   return formatedEvents
+    await writeFile('event-deeplinks.json', JSON.stringify(formatedEvents, null, 2));
     
   } catch (error) {
     console.error('Error fetching events:', error.message);
   }
 }
 
-// fetchDeeplinks();
-fetchEvents();
+fetchDeeplinks();
+// fetchEvents();
 
 // Run the function
